28062b7624ad6e837ba0ff041f6410d1e7859867
MCME Admin <sorcrar@gmail.com>
2026-01-22 14:54:42 -0600

    Agregar cambios y carpetas

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,12 @@
 const { Pool } = require("pg");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: isProduction
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 module.exports = pool;
