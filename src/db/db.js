eea96ef01ece6d69682a676d7d2d579d91570529
MCME Admin <sorcrar@gmail.com>
2026-01-22 15:00:48 -0600

    Feat: cambios en db y server

--- src/db/db.js
+++ src/db/db.js
@@ -1,12 +1,10 @@
 const { Pool } = require("pg");
 
-const isProduction = process.env.NODE_ENV === "production";
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProduction
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
