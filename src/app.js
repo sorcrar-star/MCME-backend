52aa8504737aeff04185f8a3c7128aeec984aaca
MCME Admin <sorcrar@gmail.com>
2026-01-22 17:20:43 -0600

    fix: inicializar app antes de usar rutas

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+
+// rutas
+const authRoutes = require("./routes/auth.routes");
 const adminRoutes = require("./routes/admin.routes");
 
-app.use("/api/admin", adminRoutes);
-
-const authRoutes = require("./routes/auth.routes");
-
+// inicializar app PRIMERO
 const app = express();
 
+// middlewares
 app.use(cors());
 app.use(express.json());
 
+// endpoints
 app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
 
 app.get("/", (req, res) => {
   res.json({ status: "Backend funcionando correctamente" });
